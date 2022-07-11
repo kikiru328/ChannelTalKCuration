7e2a5d26072de3f6fd93c967e2136eca6da05ab7
김광훈 <danielkim@gimgwanghun-ui-MacBookPro.local>
2022-07-11 20:56:28 +0900

    flask_webhook_success

--- A.py
+++ A.py
@@ -14,12 +14,32 @@ def webhook():
 
     '''자동화 작업 함수'''
     def auto(content):
-
-        with open(f'content.json','w',encoding='UTF-8-sig') as file_:
-            json.dump(content,file_,ensure_ascii = False,indent=4)
-            # file_.write('\n')
-    
-
+        import Util        
+        import time
+        # with open('./content.json','w') as file_:
+        #     json.dump(content,file_)
+        chat_ID = content.get('refers').get('message')['chatId']
+        print(chat_ID)
+        thirdparty = content.get('refers').get('user')['profile']['thirdPartyAgree']
+        print(thirdparty)
+        if thirdparty != True:
+            pass
+        else:
+            time.sleep(2)
+            Util.first_block(chat_ID, content)
+            time.sleep(2)
+            Util.second_block(chat_ID, content)
+            time.sleep(0.5)
+            Util.second_1_block(chat_ID,content)
+            time.sleep(2)
+            Util.third_block(chat_ID,content)
+            time.sleep(2)
+            Util.fourth_block(chat_ID,content)
+            time.sleep(2)
+            Util.fifith_block(chat_ID,content)
+            time.sleep(2)
+            Util.final_block(chat_ID)
+        
     if request.method == 'GET':
         content = request.args.to_dict()
     elif request.method == 'POST':
@@ -36,4 +56,5 @@ def webhook():
 
 
 if __name__ == '__main__':
+    # app.run(port=80, debug=True)
     app.run(port=80, debug=True)
